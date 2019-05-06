888e9cd110b285ca3525be0f48030a7ce67c331b
OWEN WILLIAMS <owen.williams22@clcmbp29.local>
2019-05-06 09:15:36 -0500

    negro

--- tiger piano tiles/GameScene.swift
+++ tiger piano tiles/GameScene.swift
@@ -18,11 +18,12 @@ class GameScene: SKScene {
     
     
     var tile = SKSpriteNode(imageNamed: "baboom")
-    
+   // var tileorange = SKSpriteNode()  //wait on this
     
     override func didMove(to view: SKView) {
         
-        TileMaker()
+            TileMaker()
+        
         print("hi")
         backgroundColor = UIColor.black
         
@@ -30,16 +31,37 @@ class GameScene: SKScene {
         
         }
     
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
     func TileMaker(){
         
-        tile.position = CGPoint(x: 47, y: self.size.height)
+        tile.position = CGPoint(x: self.size.width, y: random(min: 0, max: self.size.height))
         tile.physicsBody = SKPhysicsBody(rectangleOf: tile.size)
         tile.physicsBody?.pinned = true
         tile.physicsBody?.affectedByGravity = false
-        tile.scale(to: CGSize(width: 93.441, height: 334.946))
+        tile.scale(to: CGSize(width: 93.441, height: 200))
         
         addChild(tile)
+        
     }
+    
+    
+   /* func orangetile(){
+        backgroundColor = UIColor.orange
+        
+        
+        
+    }
+    */
+    
+    
+    
+    
     
     
     
